a93f55610a8bab3675fe8a5735a14486d414d76f
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2021-08-30 16:12:56 -0700

    create the shiny app with two tabs

---
+++ shiny_key.R
@@ -0,0 +1,45 @@
+# Load packages
+library(shiny) #this will allow you to create a shiny app
+library(tidyverse) #this will allow you to wrangle data
+library(shinythemes) #this will allow you to make your shiny app a different theme
+
+# Read in data
+penguins <- read_csv("penguins.csv")
+
+# Create the ui (user interface) - what the user sees 
+ui <- navbarPage("My navigation bar", #create page with navigation bar on top
+                 theme = shinytheme("cyborg"), #personalize your app with a theme 
+                 tabPanel("First tab is the first tab", #create a new tab
+                          sidebarLayout( #create a sidebar within the tab
+                            sidebarPanel("put some text here!", #add a title to the sidebar
+                                         radioButtons(inputId = "species", #create a radiobutton with options from "species" (THIS IS PART OF THE SERVER)
+                                                      label = "pick a species:", #label it (THIS IS PART OF THE UI)
+                                                      choices = c("Adelie", "Gentoo", "Awesome Chinstraps"="Chinstrap")), #format choices in same exact format as they show up in 'sp_short' column of the penguins dataframe, add = if you want to change what it looks like to user
+                                         selectInput(inputId = "pt_color", #create a dropdown menu with options from "pt_color" (THIS IS PART OF THE SERVER)
+                                                     label = "pick a point color", #label it (THIS IS PART OF THE UI)
+                                                     choices = c("favorite RED!!"="red", "pretty purple!"="purple", "ORAAAANGE!!!"="orange"))), #remember, add = if you want to change what it looks like to user vs what R reads
+                            mainPanel("some more text is here", #add some text to the main panel
+                                      plotOutput(outputId = "penguin_plot")))), #add a 'penguin_plot' to the main panel (THIS IS PART OF THE SERVER)
+                 tabPanel("Second tab is the second tab", #create a new tab
+                            mainPanel("some more text is here", #add some text to the main panel
+                                      img(src='myImage.jpg', align = "center"))) #add 'myImage' to the main panel, align it to the center of the page
+                          )
+
+#Create the server - what the app developer sees (what's going on behind the scenes)
+server <- function(input,output) {
+  
+  #create a reactive ({}) data frame
+  penguin_select <- reactive({ #create a reactive data frame called "penguin_select"
+    penguins %>% #use the "penguins" data frame AND THEN
+      filter(sp_short==input$species) #filter for species in the 'sp_short' column that the user (in the UI) selects in the 'species' widget
+  })
+  
+  #create a reactive ({}) output using a reactive data frame()
+  output$penguin_plot <- renderPlot({ #create a reactive plot called "penguin_plot" - it's a reactive plot because it changes depending on the data frame
+    ggplot(data=penguin_select(), aes(x=flipper_length_mm, y=body_mass_g)) + #create a ggplot using the reactive data frame, "penguin_select"
+      geom_point(color=input$pt_color) #change the color of the points depending on what the user (in the UI) selects in the 'pt_color' widget
+  })
+}
+
+# Tell R you want to create a Shiny app, with the user interface (ui) and the server (server)
+shinyApp(ui=ui, server=server)
